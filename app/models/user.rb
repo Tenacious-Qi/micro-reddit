9e85bc2566b95c475adde5ffd303e08a41bd98af
Tenacious-Qi <joe.mccann@mailbox.org>
2021-01-11 17:50:37 -0500

    generate user model. save first user to db

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# data model for users of micro-reddit service
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true,
+                       length: { in: 3..20, message: 'username must be between 3 and 20 characters' }
+  validates :password, presence: true,
+                       length: { in: 8..50, message: 'password must be between 8 and 50 characters' }
+end
