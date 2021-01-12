3ad74eb710c2889f7d01c50897c6d4b143f11da4
Tenacious-Qi <joe.mccann@mailbox.org>
2021-01-11 20:40:36 -0500

    create table for CreatePosts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
+
 class Post < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, presence: true, 
+                    length: { in: 1..300, message: 'title must be between 1 and 300 characters' }
+  validates :link, presence: true
+  validates_format_of :link, 
+                      :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
 end
